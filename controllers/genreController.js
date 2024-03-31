8d2190d138baacdebf1913dbaa4c6cf61dd8cc06
dielawn <dielawnmerkill@gmail.com>
2024-03-31 08:14:57 -0600

    genre list page with links to genre details pages

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -3,7 +3,8 @@ const asyncHandler = require('express-async-handler');
 
 //display list of all Genre
 exports.genre_list = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Genre list');
+    const allGenres = await Genre.find().sort({ name: 1 }).exec();
+    res.render('genre_list', { title: 'Genre List', genre_list: allGenres });
 });
 
 //display detail page for a specific Genre
