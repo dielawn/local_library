aa0b2753c7d9e681533333d6aafee9a689f52b76
dielawn <dielawnmerkill@gmail.com>
2024-03-31 08:29:49 -0600

    book details pages for each book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -55,7 +55,22 @@ exports.book_list = asyncHandler(async (req, res, next) => {
 });
 //display detail page for a specific Book
 exports.book_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Book ${req.params.id}`);
+    const [book, bookInstances] = await Promise.all([
+        Book.findById(req.params.id).populate('author').populate('genre').exec(),
+        BookInstance.find({ book: req.params.id }).exec(),
+    ]);
+
+    if (book === null) {
+        const err = new Error('Book not found');
+        err.status = 404;
+        return new(err);
+    }
+
+    res.render('book_detail', {
+        title: book.title,
+        book: book,
+        book_instances: bookInstances,
+    });
 });
 
 //update
